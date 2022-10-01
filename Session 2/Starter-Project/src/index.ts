d2f8b8538b764c9fac00593ca1d3b7962b1d2af5
unknown <mohaned.ashraf.elsayed@gmail.com>
2022-10-01 12:16:56 +0200

    add eslint

--- Session 2/Starter-Project/src/index.ts
+++ Session 2/Starter-Project/src/index.ts
@@ -1,3 +1,16 @@
-let firstName: string = "mohaned";
+type Person = {
+  firstName: string;
+  readonly lastName: string;
+};
+type Person2 = Person & {
+  age: number;
+};
+const person1: Person2 = { firstName: "mohaned", lastName: "ashraf", age: 25 };
 
-console.log(firstName);
+if (person1.age === 21) {
+  console.log("s");
+}
+
+const mohaned = "mohaned";
+
+console.log(person1);
