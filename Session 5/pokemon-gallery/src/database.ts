798c5590d169f47bd76aea9f0c9ef85ecf1a0f4e
unknown <mohaned.ashraf.elsayed@gmail.com>
2022-10-22 11:19:02 +0200

    session-5 env variables

--- Session 5/pokemon-gallery/src/database.ts
+++ Session 5/pokemon-gallery/src/database.ts
@@ -1,10 +1,14 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const client = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'postgres',
-  database: 'udacity',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 export default client;
