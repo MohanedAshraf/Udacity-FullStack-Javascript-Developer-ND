e88919c7b24c28914d8f3f3affaf1e4ce3a52068
unknown <mohaned.ashraf.elsayed@gmail.com>
2022-10-22 11:49:10 +0200

    session-5 app structure

--- Session 5/pokemon-gallery/src/index.ts
+++ Session 5/pokemon-gallery/src/index.ts
@@ -1,16 +1,10 @@
 import express from 'express';
-import client from './database';
+
+import routes from './routes';
 
 const app = express();
 const port = 3000;
-
-app.get('/', async (req, res) => {
-  const conn = await client.connect();
-  const query = 'SELECT * FROM pokemons';
-  const { rows } = await conn.query(query);
-  conn.release();
-  res.send(rows);
-});
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
