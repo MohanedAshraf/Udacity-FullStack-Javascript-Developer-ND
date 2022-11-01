480c71fc9234be4f2a0e3ae06af501b28dc45ec4
unknown <mohaned.ashraf.elsayed@gmail.com>
2022-11-01 21:37:08 +0200

    session-6 issue with contactInfo route

--- Session 6/pokemon-gallery/src/routes/index.ts
+++ Session 6/pokemon-gallery/src/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import pokemonsRoute from './api/pokemons';
-import contactInfoRoute from './api/ContactInfo'
-import usersRoute from './api/users'
-import galleriesRoute from './api/galleries'
+import contactInfoRoute from './api/contactInfo';
+import usersRoute from './api/users';
+import galleriesRoute from './api/galleries';
 
 const routes = Router();
 
 routes.use('/pokemons', pokemonsRoute);
-routes.use('/contactinfo', contactInfoRoute);
 routes.use('/users', usersRoute);
+routes.use('/users/contactinfo', contactInfoRoute);
 routes.use('/users/galleries', galleriesRoute);
 
 export default routes;
