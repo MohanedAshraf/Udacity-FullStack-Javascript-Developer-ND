71581997344d7e544814068de14ac83045327dc3
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2022-11-05 08:37:53 +0200

    session-7 add model test

--- Session 7/pokemon-gallery/src/database.ts
+++ Session 7/pokemon-gallery/src/database.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME ,TEST_DB_NAME , ENV} = process.env;
 const client = new Pool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_NAME,
+  database: ENV === 'dev' ? DB_NAME : TEST_DB_NAME,
 });
 
 export default client;
